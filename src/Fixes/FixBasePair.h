7b56ba54214815ec1a6f3e32781a11f6c5e9fd60
jmoller93 <jmoller93@gmail.com>
2016-08-23 11:26:40 -0500

    Added in fix files for base pairs

---
+++ src/Fixes/FixBasePair.h
@@ -0,0 +1,34 @@
+#pragma once
+#ifndef FIXBASEPAIR_H
+#define FIXBASEPAIR_H
+
+#include <boost/python.hpp>
+
+#include "FixPotentialMultiAtom.h"
+#include "BasePair.h"
+#include "BasePairEvaluate.h"
+
+void export_FixBasePair();
+
+class FixBasePair : public FixPotentialMultiAtom<BasePairVariant, BasePair, BasePair, BasePairGPU, BasePairType, 4> {
+
+private:
+    BasePairEvaluator evaluator;
+public:
+    //DataSet *eng;
+    //DataSet *press;
+
+    FixBasePair(boost::shared_ptr<State> state_, std::string handle);
+
+    void compute(bool);
+    void singlePointEng(float *);
+
+    void createBasePair(Atom *, Atom *, Atom *, Atom *, double, double, double, double, double,  int);
+    void setBasePairTypeCoefs(int, double, double, double, double, double);
+
+    //std::vector<pair<int, std::vector<int> > > neighborlistExclusions();
+    bool readFromRestart(pugi::xml_node restData);
+
+};
+
+#endif
